cb86fb93e5cbd7cfd8f74bcde7b9abf7905ca9ce
David Siebert <david.siebert@outlook.de>
2020-09-29 12:45:59 +0200

    Anlage gRPC Server Projekt

---
+++ DataInputt/gRpcServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using System;
+
+namespace gRpcServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 9000;
+            const string host = "0.0.0.0";
+
+            Server server = new Server
+            {
+                Ports = { new ServerPort(host, port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine($"Starting server {host}:{port}");
+            Console.WriteLine("Press any key to stop...");
+            Console.ReadKey();
+        }
+    }
+}
